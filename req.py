f6286c45f9c7adaeaaa874da05cd34ba58636c90
Valentina Lyakhovich <levva.tsepesh@gmail.com>
2021-02-14 03:28:02 +0300

    Testing change add_user, fixture, function

--- req.py
+++ req.py
@@ -1,50 +1,45 @@
 import requests
-import sys
 
-#
-#
-# """
-# Example of a request to add a user with name = TestName and surname = TestSurname:
-# http://127.0.0.1:8000/edit?id=3&name=TestName&surname=TestSurname
-# """
-#
-#
-# r = requests.get('http://127.0.0.1:8000/add?name=TestName&surname=TestSurname')
-# print(r.status_code)
-# print(r.json())
-#
-#
-# """
-# Example of a search request for a user with id = 2:
-# http://127.0.0.1:8000/get?id=2
-# """
-#
-# r = requests.get(f'http://127.0.0.1:8000/get?id={sys.maxsize}')
-# print(r.status_code)
-# print(r.json())
-#
-#
-# """
-# An example of a request to update user with id = 3 for name = TestName and surname = TestSurname:
-# http://127.0.0.1:8000/edit?id=3&name=TestName&surname=TestSurname
-# """
-#
-#
-# r = requests.get('http://127.0.0.1:8000/edit?id=3&name=TestName&surname=TestSurname')
-# print(r.status_code)
-# print(r.json())
-#
-#
-# """
-# An example of a request to delete user a user with id = 4:
-# http://127.0.0.1:8000/delete?id=4
-# """
-#
-# r = requests.get(f'http://127.0.0.1:8000/delete?id=4')
-# print(r.status_code)
-# print(r.json())
-#
-r = requests.post('http://127.0.0.1:8000/items/5',
-                  json={"name": "string", "surname": "string", 'bool_value': 0})
+"""
+Example of a request to add a user with name = TestName and surname = TestSurname:
+"""
+
+r = requests.post('http://127.0.0.1:8000/add',
+                  json={'name': "TestName", 'surname': "TestSurname"})
+print(r.status_code)
+print(r.json())
+
+"""
+Example of a search request for a user with id = 2:
+"""
+
+r = requests.get(f'http://127.0.0.1:8000/get?id=2')
+print(r.status_code)
+print(r.json())
+
+"""
+An example of a request to update user with id = 3 for name = TestName and surname = TestSurname:
+"""
+
+r = requests.put('http://127.0.0.1:8000/edit/3',
+                 json={'name': "TestName", 'surname': "TestSurname"})
+print(r.status_code)
+print(r.json())
+
+r = requests.put('http://127.0.0.1:8000/edit',
+                 json={'id': 3, 'name': "TestName", 'surname': "TestSurname"})
+print(r.status_code)
+print(r.json())
+
+"""
+An example of a request to delete user a user with id = 4:
+"""
+
+r = requests.delete('http://127.0.0.1:8000/delete/4')
+print(r.status_code)
+print(r.json())
+
+r = requests.delete('http://127.0.0.1:8000/delete',
+                    json={'id': 4})
 print(r.status_code)
 print(r.json())
--- serv.py
+++ serv.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
-from typing import Optional
 from fastapi import FastAPI
 import uvicorn
 import json
@@ -30,14 +29,16 @@ def csv_writer(some_data):
     try:
         with open(global_param.FILE, "a+") as csv_file:
             writer = csv.DictWriter(csv_file, delimiter=',', lineterminator='\r', fieldnames=global_param.FIELDNAMES)
+            users_state = csv_reader()
             for row in some_data:
-                if len(csv_reader()) == 0:
+                if len(users_state) == 0:
+                    row['id'] = 1
                     writer.writeheader()
+                elif len(users_state) == 1:
                     row['id'] = 1
                 else:
-                    row['id'] = int(csv_reader()[-1][0]) + 1
+                    row['id'] = int(users_state[-1][0]) + 1
                 writer.writerow(row)
-        csv_file.close()
         return True
     except:
         return False
@@ -51,14 +52,14 @@ def get_user(id):
     result = []
     list_of_user = csv_reader()
     if len(list_of_user) == 0:
-        return 'No users have been added yet'
+        return {'warning': 'No users have been added yet'}
     else:
         list_of_user = list_of_user[1:]
     for row in list_of_user:
         if int(row[0]) == id:
             result.append([row[1], row[2]])
     if len(result) == 0:
-        return f'Could not find user with id={id}'
+        return {'error': f'User with id={id} not found'}
     elif len(result) > 1:
         dict_res = {}
         for i in range(len(result)):
@@ -73,7 +74,7 @@ def edit_user(id, name='', surname=''):
     list_of_user = csv_reader()
 
     if len(list_of_user) == 0:
-        return 'No users have been added yet'
+        return {'warning': 'No users have been added yet'}
     else:
         list_of_user = list_of_user[1:]
 
@@ -89,16 +90,16 @@ def edit_user(id, name='', surname=''):
         csv_update(list_of_user)
         return True
     else:
-        return f'User with id={id} not found'
+        return {'error': f'User with id={id} not found'}
 
 
 def delete_user(id):
     list_of_user = csv_reader()
     deleted_element = 0
     if len(list_of_user) == 0:
-        return 'No users have been added yet'
+        return {'warning': 'No users have been added yet'}
     else:
-        list_of_user = list_of_user[1:]
+        list_of_user = list(list_of_user[1:])
         count = 0
         for i in range(len(list_of_user)):
             if int(list_of_user[i][0]) == id:
@@ -110,7 +111,7 @@ def delete_user(id):
         csv_update(list_of_user)
         return True
     else:
-        return f'User with id={id} not found'
+        return {'error': f'User with id={id} not found'}
 
 
 def csv_update(some_data):
@@ -130,32 +131,21 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    surname: str
-    bool_value: Optional[bool] = None
+    name: str = ''
+    surname: str = ''
+    id: int = 0
 
 
-@app.post("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"Name": item.name, "Surname": item.surname, "item_id": item_id}
-
-
-@app.post('/add')
-def method(**kwargs):
-    for arg in kwargs:
-        return arg
-
-
-@app.get('/add')
-def method(name: str = '', surname: str = ''):
-    if not (name and surname):
-        return '2 required parameters are expected: name and surname'
-    some_data = [{'name': name, 'surname': surname}]
+@app.post("/add")
+def method(item: Item):
+    if not (item.name or item.surname):
+        return {'error': '2 required parameters are expected: name and surname'}
+    some_data = [{"name": item.name, "surname": item.surname}]
     add_status = csv_writer(some_data)
     if add_status:
-        return f'User {name} {surname} successfully added'
+        return {'status': f'User {item.name} {item.surname} successfully added'}
     else:
-        return 'Unable to access the user list'
+        return {'error': 'Unable to access the user list'}
 
 
 @app.get('/get')
@@ -166,31 +156,48 @@ def method(id: int = 0):
     return user_data
 
 
-@app.get('/edit')
-def method(id: int, name='', surname=''):
-    if not id:
-        return '1 required parameter are expected: id'
-    edit_status = edit_user(id, name, surname)
+@app.put('/edit/{id}')
+def method(id: int, item: Item):
+    if id == 0:
+        return {'error': 'ID must be more then 0'}
+    edit_status = edit_user(id, item.name, item.surname)
     if edit_status == True:
-        return f'User with id={id} successfully edited'
+        return {'status': f'User with id={id} successfully edited'}
     else:
         return edit_status
 
 
-@app.get('/delete')
+@app.put('/edit')
+def method(item: Item):
+    if item.id == 0:
+        return {'error': 'ID must be more then 0'}
+    edit_status = edit_user(item.id, item.name, item.surname)
+    if edit_status == True:
+        return {'status': f'User with id={item.id} successfully edited'}
+    else:
+        return edit_status
+
+
+@app.delete('/delete/{id}')
 def method(id: int):
-    if not id:
-        return '1 required parameter are expected: id'
+    if id == 0:
+        return {'error': 'ID must be more then 0'}
     delete_status = delete_user(id)
     if delete_status == True:
-        return f'User with id={id} successfully deleted'
+        return {'status': f'User with id={id} successfully deleted'}
     else:
         return delete_status
 
 
-@app.post('/')
-def method():
-    pass
+@app.delete('/delete')
+def method(item: Item):
+    if item.id == 0:
+        return {'error': 'ID must be more then 0'}
+    delete_status = delete_user(item.id)
+    if delete_status == True:
+        return {'status': f'User with id={item.id} successfully deleted'}
+    else:
+        return delete_status
 
 
 if __name__ == "__main__":
